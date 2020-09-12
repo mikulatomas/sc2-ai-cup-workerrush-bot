2b5291abb2b65b242d97c7f2ee7649980b45f2be
Tomáš Mikula <mail@tomasmikula.cz>
2020-09-12 18:41:36 +0200

    Fix import

--- workerrush_bot/workerrush_bot.py
+++ workerrush_bot/workerrush_bot.py
@@ -1,4 +1,7 @@
-class WorkerRushBot(sc2.BotAI):
+from sc2.bot_ai import BotAI
+
+
+class WorkerRushBot(BotAI):
     async def on_step(self, iteration):
         if iteration == 0:
             for worker in self.workers:
